eb692693fbd5410bd7d667a893f278d09b76f582
thefourthway <175942003+thefourthway@users.noreply.github.com>
2025-08-31 02:43:56 +0000

    fc

---
+++ staged_async.py
@@ -0,0 +1,344 @@
+import functools
+import typing
+
+
+class _ApprovalUnset:
+    pass
+
+
+class _DeadGenerator:
+    pass
+
+
+class _ItemUnset:
+    pass
+
+
+class _ResultUnset:
+    pass
+
+
+# The final return value of our SAO, stored in SAO._result
+_R = typing.TypeVar('_R')
+
+
+# The generator returned by the client function
+AsyncClientEventGeneratorT: typing.TypeAlias = "typing.AsyncGenerator[_OperationEvent | _R, _OperationEvent]"
+
+# The generator returned by SAOGenerator.generate
+AsyncEventGeneratorT: typing.TypeAlias = "typing.AsyncGenerator[_OperationEvent, None]"
+
+
+# The stored value of the previous item returned by the client generator. type[_ItemUnset] is the initial state of SAOGenerator._previous_yield
+_GeneratorItemState: typing.TypeAlias = "_OperationEvent | _R | type[_ItemUnset]"
+
+# The type of the function we are passing our SAO into
+
+
+class SAOClientCallable(typing.Protocol[_R]):
+    def __call__(self, sao: 'SAO[_R]',
+                 *args: typing.Any, **kwargs: typing.Any) -> 'AsyncClientEventGeneratorT[_R]': ...
+
+
+# The "awaitable" variant where all events are folded at once
+SAOResultAwaitable: typing.TypeAlias = 'typing.Awaitable[_R]'
+
+# The "generator" type used by default_sao
+SAOAwaitGenerator: typing.TypeAlias = 'typing.Generator[typing.Any, typing.Any, _R]'
+
+# The event iterator type for SAO.__call__
+SAOEventIterator: typing.TypeAlias = "typing.AsyncIterator[tuple[SAO[_R], _OperationEvent]]"
+
+
+class SAOAwaitableSuperposition(typing.Protocol[_R]):
+    def __await__(self) -> 'SAOAwaitGenerator[_R]': ...
+    def __aiter__(self) -> 'SAOEventIterator[_R]': ...
+
+
+class _SAOSuperposition(typing.Generic[_R]):
+    def __init__(self, _quantum_state: typing.Any):
+        self._quantum_state = _quantum_state
+
+    def __await__(self) -> 'SAOAwaitGenerator[_R]':
+        quantum_state = object.__getattribute__(self, '_quantum_state')
+
+        return typing.cast('SAOAwaitGenerator[_R]', quantum_state.__await__())
+
+    def __aiter__(self) -> 'SAOEventIterator[_R]':
+        quantum_state = object.__getattribute__(self, '_quantum_state')
+
+        return typing.cast('SAOEventIterator[_R]', quantum_state.__aiter__())
+
+    def __getattribute__(self, name: str) -> typing.Any:
+        if hasattr(self, name):
+            return object.__getattribute__(self, name)
+
+        quantum_state = object.__getattribute__(self, '_quantum_state')
+
+        return getattr(quantum_state, name)
+
+
+class _OperationEvent:
+    def __init__(self, stage_id: str, event_id: str, event_args: dict[str, typing.Any], stage_context: dict[str, typing.Any]):
+        self._stage_id = stage_id
+        self._event_id = event_id
+        self._event_args: dict[str, typing.Any] = event_args
+        self._stage_context = stage_context
+
+        self._approval_status: type[_ApprovalUnset] | bool = _ApprovalUnset
+        self._allow_args: tuple[typing.Any, ...] | None = None
+
+    def approve(self, *args: typing.Any):
+        self._approval_status = True
+        self._allow_args = args
+
+    def cancel(self):
+        self._approval_status = False
+        self._allow_args = None
+
+    @property
+    def stage_id(self):
+        return self._stage_id
+
+    @property
+    def event_id(self):
+        return self._event_id
+
+    @property
+    def event_args(self):
+        return self._event_args
+
+    @property
+    def stage_context(self):
+        return self._stage_context
+
+    @property
+    def caller_args(self) -> tuple[typing.Any, ...] | None:
+        return self._allow_args
+
+    @property
+    def has_been_approved(self) -> bool:
+        return self._approval_status is True
+
+    @property
+    def has_been_stamped(self) -> bool:
+        return self._approval_status != _ApprovalUnset
+
+    def __hash__(self) -> int:
+        return hash(self._stage_id)
+
+    def __repr__(self) -> str:
+        return f'OperationEvent(id={repr(self._stage_id)}, kwargs={repr(self.event_args)})'
+
+    def __bool__(self):
+        return self.has_been_approved
+
+    def __str__(self):
+        return repr(self)
+
+
+class _OperationStage:
+    """
+    Usage:
+
+    with sao.stage('DoSomething', start_account_id=1) as stage:
+        ev = yield stage.allowed("try_set_db_field", field_name='account_id', field_value=new_account_id)
+
+    """
+
+    def __init__(self, stage_id: str, stage_context: dict[str, typing.Any]):
+        self._id = stage_id
+        self._context = stage_context
+
+    def allowed(self, event_id: str, **kwargs: typing.Any):
+        return _OperationEvent(self._id, event_id, kwargs, self._context)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *_):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        pass
+
+
+class _SAOGenerator(typing.Generic[_R]):
+    def __init__(self, sao: 'SAO[_R]', fn: 'SAOClientCallable[_R]', *args: typing.Any, **kwargs: typing.Any):
+        self._sao: 'SAO[_R] | None' = sao
+        self._fn: 'SAOClientCallable[_R] | None' = fn
+        self._fn_args: 'tuple[typing.Any, ...] | None' = args
+        self._fn_kwargs: 'dict[str, typing.Any] | None' = kwargs
+
+        self._fn_async_generator: 'AsyncClientEventGeneratorT[_R] | type[_DeadGenerator] | None' = None
+        self._previous_yield: '_GeneratorItemState[_R]' = _ItemUnset
+
+    def _invalidate(self):
+        self._sao = None
+        self._fn = None
+        self._fn_args = None
+        self._fn_kwargs = None
+        self._fn_async_generator = _DeadGenerator
+        self._previous_yield = _ItemUnset
+
+    async def generate(self) -> AsyncEventGeneratorT:
+        if self._sao is None or self._fn is None or self._fn_args is None or self._fn_kwargs is None:
+            raise RuntimeError('The SAO that can be re-entered is not the true SAO')
+
+        if self._fn_async_generator == _DeadGenerator:
+            raise RuntimeError('The SAO that can be re-entered is not the true SAO')
+
+        if self._fn_async_generator is None:
+            self._fn_async_generator = self._fn(
+                self._sao,
+                *self._fn_args,
+                **self._fn_kwargs
+            )
+
+        if self._fn_async_generator == _DeadGenerator:
+            raise RuntimeError('The SAO that can be re-entered is not the true SAO')
+
+        gen: 'AsyncClientEventGeneratorT[_R]' = typing.cast(
+            'AsyncClientEventGeneratorT[_R]',
+            self._fn_async_generator
+        )
+
+        item: _OperationEvent | _R
+        send: _OperationEvent | None = None
+
+        try:
+            while True:
+                try:
+                    if send is None:
+                        item = await anext(gen)
+                    else:
+                        item = await gen.asend(send)
+                        send = None
+                except StopAsyncIteration:
+                    break
+
+                self._previous_yield = item
+
+                if isinstance(item, _OperationEvent):
+                    yield item
+
+                    if not item.has_been_stamped:
+                        item.approve()
+
+                    if item.has_been_approved:
+                        send = item
+                    else:
+                        raise StopAsyncIteration
+        except Exception:
+            raise
+        finally:
+            final_item = self._previous_yield
+
+            if not isinstance(final_item, _OperationEvent) and final_item != _ItemUnset:
+                # Just hope for the best that the type is _R, can't really do much about it
+                self._sao._result = typing.cast(_R, final_item)
+
+            self._invalidate()
+
+
+class SAO(typing.Generic[_R]):
+    def __init__(self) -> None:
+        self._result: '_R | type[_ResultUnset]' = _ResultUnset
+
+        self._sao_generator_obj: '_SAOGenerator[_R] | None' = None
+        self._event_generator: 'AsyncEventGeneratorT | None' = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self._invalidate_generator()
+
+    def stage(self, stage_id: str, **kwargs: typing.Any) -> _OperationStage:
+        return _OperationStage(stage_id, kwargs)
+
+    def result(self) -> _R:
+        if self._result == _ResultUnset:
+            raise RuntimeError(
+                'No result was returned by the client generator')
+
+        return typing.cast(_R, self._result)
+
+    def _invalidate_generator(self):
+        self._sao_generator_obj = None
+        self._event_generator = None
+
+    def __call__(self, fn: 'SAOClientCallable[_R]', *args: typing.Any, **kwargs: typing.Any) -> 'SAOEventIterator[_R]':
+        if self._sao_generator_obj is not None or self._event_generator is not None:
+            raise RuntimeError('SAO has already been initialized')
+
+        if self._result != _ResultUnset:
+            self._result = _ResultUnset
+
+        async def gen():
+            if self._sao_generator_obj is not None or self._event_generator is not None:
+                raise RuntimeError(
+                    'SAO may only have one iterator client at a time')
+
+            self._sao_generator_obj = _SAOGenerator(self, fn, *args, **kwargs)
+            self._event_generator = self._sao_generator_obj.generate()
+
+            try:
+                async for item in self._event_generator:
+                    yield self, item
+            except Exception:
+                raise
+            finally:
+                self._invalidate_generator()
+
+        return gen()
+
+
+async def complete_sao_call(fn: 'SAOClientCallable[_R]', *args: typing.Any, **kwargs: typing.Any) -> '_R':
+    sao_obj = SAO[_R]()
+
+    async for sao_obj, event in sao_obj(fn, *args, **kwargs):
+        event.approve()
+
+    return sao_obj.result()
+
+
+def iterate_sao(fn: 'SAOClientCallable[_R]', *args: typing.Any, **kwargs: typing.Any) -> 'SAOEventIterator[_R]':
+    return SAO[_R]()(fn, *args, **kwargs)
+
+
+def fold_sao(fn: 'SAOClientCallable[_R]', *args: typing.Any, **kwargs: typing.Any) -> 'SAOResultAwaitable[_R]':
+    return complete_sao_call(fn, *args, **kwargs)
+
+
+def default_sao(fn: 'SAOClientCallable[_R]') -> 'typing.Callable[..., SAOAwaitableSuperposition[_R]]':
+    """
+    This will provide an extra overload for fn so that it can be called either as an event generator, or an awaitable
+    that returns sao.result when the generator completes.
+    """
+    def wrap_factory(realized_fn: 'SAOClientCallable[_R]') -> 'typing.Callable[..., SAOAwaitableSuperposition[_R]]':
+        """
+        @param: realized_fn - The SAO that can be realized is not the true SAO
+        """
+        def provide_default_sao(*args: typing.Any, **kwargs: typing.Any) -> 'SAOAwaitableSuperposition[_R]':
+            if len(args) >= 2 and isinstance(args[0], SAO):
+                sao_obj = typing.cast(SAO[_R], args[0])
+                rest = args[2:]
+
+                # here, someone has called sao(provide_default_sao), opting to provide their own event handlers.
+                # args[1] under normal circumstances will just be provide_defaultSAO, so we skip it
+
+                res: 'SAOEventIterator[_R]' = sao_obj(
+                    realized_fn, *rest, **kwargs
+                )  # type: ignore
+
+                return _SAOSuperposition(res) # type: ignore
+
+            return _SAOSuperposition(complete_sao_call(realized_fn, *args, **kwargs)) # type: ignore
+
+        return provide_default_sao
+
+    return wrap_factory(fn)
